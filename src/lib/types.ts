865136d67e53fb303072607c8b76614833292c99
shlomomdahan <64103471+shlomomdahan@users.noreply.github.com>
2024-03-18 23:22:38 -0400

    sorting viz

---
+++ src/lib/types.ts
@@ -0,0 +1,13 @@
+export type SortingAlgorithmType =
+  | "bubble"
+  | "selection"
+  | "insertion"
+  | "merge"
+  | "quick";
+
+export type SelectOptionsType = {
+  value: string;
+  label: string;
+};
+
+export type AnimationArrayType = [number[], boolean][];
